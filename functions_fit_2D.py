a39152d49b198d263cb6c688eca3761b827d87b5
Jamie Dunsmore <jamie77d@gmail.com>
2024-08-26 14:43:24 -0400

    Allowed reduced chi squared function option to only use edge chi squared

--- functions_fit_2D.py
+++ functions_fit_2D.py
@@ -1333,8 +1333,14 @@ def reduced_chi_squared(ydata, yfit, yerr, num_params):
     return chi_squared(ydata, yfit, yerr) / (len(ydata) - num_params)
 
 
-def reduced_chi_squared_inside_separatrix(psi_values, ydata, yfit, yerr, num_params):
-    inside_separatrix_mask = psi_values < 1.0
+def reduced_chi_squared_inside_separatrix(psi_values, ydata, yfit, yerr, num_params, only_edge = False):
+    '''
+    If only edge mask is true, will just calculate the reduced chi squared between psi = 0.6 and psi = 1
+    '''
+    if only_edge == True:
+        inside_separatrix_mask = (psi_values > 0.6) & (psi_values < 1.0)
+    else:
+        inside_separatrix_mask = psi_values < 1.0
     ydata = ydata[inside_separatrix_mask]
     yfit = yfit[inside_separatrix_mask]
     yerr = yerr[inside_separatrix_mask]
